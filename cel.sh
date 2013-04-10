4e80e7e04de4af9723a666119651c159370d62ba
Samy Dindane <samy@dindane.com>
2013-04-10 23:45:52 +0200

    Turning the script to an executable

--- cel.sh
+++ cel.sh
@@ -1,34 +1,24 @@
-change-execute-loop () {
-  if [ "`uname`" = "Darwin" ]; then
-    checker="ls -lRT $1"
-  else
-    checker="ls -lR --full-time $1"
+#!/bin/sh
+
+if [ "`uname`" = "Darwin" ]; then
+  checker="ls -lRT $1"
+else
+  checker="ls -lR --full-time $1"
+fi
+
+command="${@: 2:$#}"
+checksum=`eval $checker`
+watched_file=$1
+
+# Execute the specified command the first time the script's called
+eval $command
+
+while true; do
+  if [ "$checksum" != "`eval $checker`" ]; then
+    checksum="`eval $checker`"
+    eval $command
   fi
 
-  if [[ "`echo $SHELL`" =~ "zsh" ]]; then
-    command="$@[2,$#]"
-  elif [[ "`echo $SHELL`" =~ "bash" ]]; then
-    command="${@: 2:$#}"
-  else
-    echo "Shell not supported. The script may not work correctly."
-    command="${@: 2:$#}"
-  fi
-
-  checksum=`eval $checker`
-  watched_file=$1
-
-  # Execute the specified command the first time the script's called
-  eval $command
-
-  while true; do
-    if [ "$checksum" != "`eval $checker`" ]; then
-      checksum="`eval $checker`"
-      eval $command
-    fi
-
-    # Wait 100 ms before each loop avoiding waking up your CPU fans
-    sleep 0.1;
-  done
-}
-
-cel () { change-execute-loop $@; }
+  # Wait 100 ms before each loop avoiding waking up your CPU fans
+  sleep 0.1;
+done
